40fcf25049858cd1a58929fd812d08f275550e97
Zheng Xuqiang <talentloong@gmail.com>
2014-03-26 17:23:59 +0800

    fixed a little bug

--- public/javascripts/account/login.js
+++ public/javascripts/account/login.js
@@ -5,17 +5,22 @@
 function login() {
   var e = $('#email').val();
   var p = $('#pwd').val();
-  $.ajax({
-    url: '/login',
-    type: 'POST',
-    data: {email: e, password: p},
-    success: function (d) {
-    },
-    error: function (rq, m) {
-      if (rq.status == 401) {
-        $('.jumbotron').append('<div class="alert alert-warning fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>'
-          + rq.responseText + '</div>');
+  if (e != '' && p != '') {
+    $.ajax({
+      url: '/login',
+      type: 'POST',
+      data: {email: e, password: p},
+      success: function (d) {
+      },
+      error: function (rq, m) {
+        if (rq.status == 401) {
+          if ($('.alert')) {
+            $('div').remove('.alert');
+          }
+          $('.jumbotron').append('<div class="alert alert-warning fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>'
+            + rq.responseText + '</div>');
+        }
       }
-    }
-  });
+    });
+  }
 }
