ba30353c1404a2a27c3bc6e315ee5574ab21ced7
Zheng Xuqiang <talentloong@gmail.com>
2014-04-13 15:15:42 +0800

    添加用户和群组列表的查看功能

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,19 @@
 package controllers;
 
 import models.account.User;
+import models.group.Group;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.listInfo;
 
 import java.util.HashMap;
 
 public class Application extends Controller {
     static HashMap<String, Object> args = new HashMap<>();
+    static User self = Account.getLoginUser();
 
     public static Result index() {
-        User self = Account.getLoginUser();
-
         args.clear();
         args.put("self", self);
 
@@ -26,5 +27,23 @@ public class Application extends Controller {
 
     public static Result test() {
         return play.mvc.Results.TODO;
+    }
+
+    public static Result userList() {
+        args.clear();
+        args.put("self", self);
+
+        args.put("users", User.finder.all());
+
+        return ok(listInfo.render("用户列表", "用户", args));
+    }
+
+    public static Result groupList() {
+        args.clear();
+        args.put("self", self);
+
+        args.put("groups", Group.finder.all());
+
+        return ok(listInfo.render("群组列表", "群组", args));
     }
 }
